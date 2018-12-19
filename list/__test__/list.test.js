275df8bfdb6e45423a306d797c5b7b16099bf94c
hingham <hannah.c.ingham@gmail.com>
2018-12-19 08:40:32 -0800

    adding work for the array methods

--- list/__test__/list.test.js
+++ list/__test__/list.test.js
@@ -6,18 +6,109 @@ const List = require('../list.js');
 
 describe('List Data Structure', () => {
 
+
   it('starts with a length of -1 and an empty data set', () => {
     let stuff = new List();
+
     expect(stuff.length).toEqual(0);
     expect(stuff.data).toEqual({});
   });
 
   it('pushes items to the end of the data set', () => {
     let stuff = new List();
+
     stuff.push('a');
     stuff.push('b');
     expect(stuff.length).toEqual(2);
     expect(stuff.data[1]).toEqual('b');
+  });
+
+  //pop method
+  it('pops off the last item off the array', () =>{
+    let stuff = new List();
+
+    stuff.push('a');
+    stuff.push('b');
+    stuff.pop();
+    expect(stuff.length).toEqual(1);
+  });
+
+  it('returns the last item of the array', () =>{
+    let stuff = new List();
+
+    stuff.push('a');
+    stuff.push('b');
+    let lastItem = stuff.pop();
+    expect(lastItem).toEqual(stuff[stuff.length]);
+  });
+
+
+  //shift method
+  it('returns the first element of an array', () => {
+    let stuff = new List();
+
+    stuff.push('a');
+    stuff.push('b');
+    let firstItem = stuff.shift();
+    console.log(stuff[0]);
+    expect(firstItem).toEqual(stuff.data[0]);
+  });
+
+  it('reduces the length of the array by 1', () => {
+    let stuff = new List();
+
+    stuff.push('a');
+    stuff.push('b');
+    stuff.shift();
+    expect(stuff.length).toEqual(1);
+  });
+
+  //unshift method
+  it('adds one or more elements to the beginning of an array', ()=> {
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+
+    stuff.unshift(['c', 'd']);
+    expect(stuff.data).toEqual({'0': 'c','1': 'd', '2': 'a','3': 'b'});
+
+  });
+
+  it('it returns the length of the new array', () =>{
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+
+    expect(stuff.unshift(['c', 'd'])).toEqual(4);
+
+  });
+
+  //splice method
+  it('changes the contents of the array', () =>{
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+
+    let newStuff = stuff.splice(1, 1, 'new');
+    expect(newStuff).toNotEqual(stuff);
+  });
+
+  it('removes exisiting elements', () =>{
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+
+    stuff.splice(1, 1, 'new');
+    expect(stuff).toEqual({'0': 'a', '1': 'new'});
+  });
+
+  it('adds elements', () =>{
+    let stuff = new List();
+    stuff.push('a');
+    stuff.push('b');
+
+    stuff.splice(1, 0, 'new');
+    expect(stuff).toEqual({'0': 'a', '1': 'new', '2': 'b'});
   });
 
 });
--- list/list.js
+++ list/list.js
@@ -28,6 +28,96 @@ class List {
     return returnValue;
   }
 
+
+  //Remove an item from the front of the array and return it's value
+  shift(){
+    let returnValue = this.data[0];
+    delete this.data[this.length];
+    this.length--;
+    return returnValue;
+  }
+
+  //unshift() adds one or more elements to the beginnin gof an array and returns the new length of the array
+
+  unshift(arr){
+    let temp = this.data;
+    let myArr = [];
+
+    for(let i = 0; i< arr.length; i++){
+      myArr[i]=arr[i];
+    }
+    for( let i = 0; i< this.length; i++){
+      myArr[i+arr.length] = this.data[i]; 
+    }
+    for(let i = 0; i<myArr.length; i++){
+      this.data[i] = myArr[i];
+    }
+    let returnValue = myArr.length;
+    return returnValue;
+  }
+
+  //splice() changes the contents of an array by removing or replacing existing elements, or adding new elements
+  splice(idx, rm, item){
+    let myTemp = this.data;
+    this.data = {};
+
+    console.log('what is this ', this);//this has a stored length of 4;
+
+    if(arguments.length===1){
+      for(let i = 0; i<myTemp.length-idx; i++){
+        this.data[i] = myTemp[i];
+      }
+    }
+  
+    if(arguments.length===2){
+
+      for(let i = 0; i<this.length-rm; i++){
+        if(idx === i){
+          console.log('looking at this ', this.push('z'));
+          this.push = myTemp[i+rm];
+          console.log(this.data);
+        }
+        else{
+          this.data[i]=myTemp[i];
+          console.log(this.data);
+        }
+      }
+    }
+
+    console.log('if only one arg ', this.data);
+    console.log('arguments ' + arguments.length);
+
+    if(arguments.length > 2){
+      for(let i = 0; i< this.length-rm; i++){
+        console.log('in for loop', i, 'idx ', idx);
+        if(idx ===i){
+          for (let j = 0; j<arguments.length -2; j++){
+            this.data[j] = arguments[j+2];
+          }
+        }
+
+        else{
+          this.data[i]=myTemp[i];
+          console.log(this.data);
+        }
+
+      }
+    }
+
+    console.log(this.data);
+  }
 }
+
+let stuff = new List();
+stuff.push('a');
+stuff.push('b');
+stuff.push('d');
+stuff.push('e');
+
+
+// console.log(stuff);
+// console.log('stuff.length' + stuff.length);
+console.log('my splice ' + stuff.splice(2, 1));
+console.log(stuff.data);
 
 module.exports = List;
