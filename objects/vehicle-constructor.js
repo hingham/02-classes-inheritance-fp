b35af04d91ad9da12ac6105d24c446f1371316c0
hingham <hannah.c.ingham@gmail.com>
2018-12-18 18:47:51 -0800

    adding the classes and factories

--- objects/vehicle-constructor.js
+++ objects/vehicle-constructor.js
@@ -30,4 +30,60 @@ Motorcycle.prototype.wheelie = () => {
   return 'Wheee!';
 };
 
-module.exports = {Car, Motorcycle};
+
+const FlyingVehicle = function(name, wheels){
+  this.name = name;
+  this.wheels = wheels;
+};
+
+FlyingVehicle.prototype.fly = () =>{
+  return 'Fly like a bird!';
+};
+
+
+FlyingVehicle.prototype.travel = () =>{
+  return 'Travels far and wide!';
+};
+
+const Helicopter = function(name){
+  FlyingVehicle.call(this, name, 0);
+};
+
+Helicopter.prototype = new FlyingVehicle; 
+
+
+const Airplane = function(name){
+  FlyingVehicle.call(this, name, 3);
+};
+
+Airplane.prototype = new FlyingVehicle; 
+
+
+Airplane.prototype.wings = () =>{
+  return 'I have wings';
+};
+
+
+const SeaCreatures = function(name, habitat){
+  this.name = name;
+  this.habitat = habitat;
+};
+
+SeaCreatures.prototype.water = () =>{
+  return 'find me in the water';
+}
+
+const Crab = function(name){
+  SeaCreatures.call(this, name, 'tidepool');
+};
+
+Crab.prototype = new SeaCreatures;
+
+const Whale = function(name){
+  SeaCreatures.call(this, name, 'Ocean');
+};
+
+Whale.prototype = new SeaCreatures;
+
+console.log(new Helicopter('heli').travel());
+module.exports = {Car, Motorcycle, Airplane, Helicopter, Crab, Whale};
